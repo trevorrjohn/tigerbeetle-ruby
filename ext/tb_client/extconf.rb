86231c705a6af7a2f87f6f5d88aaac2a3037548d
antstorm <antstorm@gmail.com>
2025-04-02 18:33:09 +0100

    Add debug info to extconf

--- ext/tb_client/extconf.rb
+++ ext/tb_client/extconf.rb
@@ -6,6 +6,10 @@ makefile_path = File.join('Makefile')
 File.open(makefile_path, 'w') do |f|
   f.puts <<~MFILE
 all:
+\tenv
+\twhich zig
+\tzig version
+\tpwd
 \tcd ./tigerbeetle && zig build clients:c -Dconfig-release=0.16.4 -Dconfig-release-client-min=0.16.4
 \tcp -rf ./tigerbeetle/src/clients/c/lib ./pkg
 MFILE
