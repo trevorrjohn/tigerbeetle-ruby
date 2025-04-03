4a89bbe207655fba79f937145de7fa0f531e8a94
antstorm <antstorm@gmail.com>
2025-04-03 13:14:46 +0100

    Remove .PHONY

--- ext/tb_client/extconf.rb
+++ ext/tb_client/extconf.rb
@@ -1,27 +1,40 @@
 #!/usr/bin/env ruby
 require 'mkmf'
 
+makefile_path = File.join('Makefile')
 client_version = '0.16.34'
 tar_package = 'pkg.tar.gz'
 
+makefile = ''
+
 if find_executable('zig') && File.exist?('./tigerbeetle/build.zig')
-  File.open(File.join('Makefile'), 'w') do |f|
-    f.puts <<~MFILE
-      .PHONY = all install clean
-      \n\n
-      all:
-      \techo "Compiling native TB client from the source"
-      \tzig version
-      \tcd ./tigerbeetle && zig build clients:c -Dconfig-release=#{client_version} -Dconfig-release-client-min=#{client_version}
-      \n\n
-      install:
-      \tcp -rf ./tigerbeetle/src/clients/c/lib ./pkg
-      \n\n
-      clean:
-      \trm -rf ./pkg/
-      \trm -rf ./tigerbeetle/src/clients/c/lib
-    MFILE
-  end
+  makefile = <<~MFILE
+    all:
+    \techo "Compiling native TB client from the source"
+    \tzig version
+    \tcd ./tigerbeetle && zig build clients:c -Dconfig-release=#{client_version} -Dconfig-release-client-min=#{client_version}
+    \n\n
+    install:
+    \tcp -rf ./tigerbeetle/src/clients/c/lib ./pkg
+    \n\n
+    clean:
+    \trm -rf ./pkg/
+    \trm -rf ./tigerbeetle/src/clients/c/lib
+  MFILE
 elsif File.exist?("./#{tar_package}")
-  `mkdir pkg && tar -xzf #{tar_package} -C ./pkg`
+  makefile = <<~MFILE
+    all:
+    \tmkdir pkg
+    \ttar -xzf #{tar_package} -C ./pkg
+    \n\n
+    install:
+    \techo "Installing precompiled native TB client"
+    \n\n
+    clean:
+    \trm -rf ./pkg/
+  MFILE
+end
+
+File.open(makefile_path, 'w') do |f|
+  f.puts makefile
 end
